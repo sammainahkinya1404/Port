6a5538d30905dcb998c8d3f9bc4ae54da64ec7aa
Samson Kinyanjui <sammainah98@gmail.com>
2024-09-08 17:02:51 +0300

    Added a Blog

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ app= Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/blog/')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/generic/')
+def generic():
+    return render_template('generic.html')
+
+@app.route('/elements/')
+def elements():
+    return render_template('elements.html')
 # run the app.
 if __name__ == "__main__":
    
